4e7dff31bb23feb13bb4ffd1d38e04a2c85f2830
RAMON <ramonb634@gmail.com>
2023-06-12 22:38:20 -0500

    Agregar archivos de proyecto.

---
+++ InventoryManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class InventoryManager
+{
+    private static InventoryManager instance;
+    private Dictionary<string, int> inventory;
+
+    private InventoryManager()
+    {
+        // Constructor privado
+        inventory = new Dictionary<string, int>();
+    }
+
+    public static InventoryManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new InventoryManager();
+            }
+            return instance;
+        }
+    }
+
+    public void AddProduct(string product, int quantity)
+    {
+        if (inventory.ContainsKey(product))
+        {
+            inventory[product] += quantity;
+        }
+        else
+        {
+            inventory.Add(product, quantity);
+        }
+        Console.WriteLine($"{quantity} {product}(s) añadido(s) al inventario.");
+    }
+
+    public void RemoveProduct(string product, int quantity)
+    {
+        if (inventory.ContainsKey(product))
+        {
+            if (inventory[product] >= quantity)
+            {
+                inventory[product] -= quantity;
+                Console.WriteLine($"{quantity} {product}(s) eliminado(s) del inventario.");
+            }
+            else
+            {
+                Console.WriteLine($"No hay suficiente cantidad de {product} en el inventario.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El producto {product} no está en el inventario.");
+        }
+    }
+
+    public int GetProductQuantity(string product)
+    {
+        if (inventory.ContainsKey(product))
+        {
+            return inventory[product];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        InventoryManager inventoryManager = InventoryManager.Instance;
+
+        inventoryManager.AddProduct("Manzanas", 10);
+        inventoryManager.AddProduct("Plátanos", 5);
+
+        Console.WriteLine($"Cantidad de manzanas en el inventario: {inventoryManager.GetProductQuantity("Manzanas")}");
+        Console.WriteLine($"Cantidad de plátanos en el inventario: {inventoryManager.GetProductQuantity("Plátanos")}");
+
+        inventoryManager.RemoveProduct("Manzanas", 3);
+
+        Console.WriteLine($"Cantidad de manzanas en el inventario después de la venta: {inventoryManager.GetProductQuantity("Manzanas")}");
+    }
+}
